610bd36057cb9175f8de836394406be41d861c92
martinklewitz <martin@klewitz.info>
2023-09-21 09:51:17 +0200

    fritzbox monitoring prometheus.yml

--- prometheus-grafana/update.sh
+++ prometheus-grafana/update.sh
@@ -4,5 +4,4 @@
 ./umbrel/scripts/repo checkout https://github.com/martinklewitz/umbrel-apps.git#prometheus
 ./umbrel/scripts/repo update
 ./umbrel/scripts/app install prometheus-grafana
-
 docker ps | grep grafana
